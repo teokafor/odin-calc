4d67e9c29d1ea199cb8be99072913a784450a689
Tobe Okafor <teokafor@gmail.com>
2024-12-21 09:38:41 -0600

    Create basic operation functions

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello world!');
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
